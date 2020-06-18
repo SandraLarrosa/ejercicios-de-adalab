ca3f489e279e6170508ae6a72c1f531dcd7ae139
Alexandra <sandra.larrosa88@gmail.com>
2020-06-18 17:58:21 +0200

    Ejercicio 4

---
+++ modulo-2/modulo-02-leccion-01-ejercicio-04/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const selection = document.querySelector('p');
+const adalaber1 = document.querySelector('.adalaber1');
+const adalaber2 = document.querySelector('.adalaber2');
+
+selection.innerHTML = selection.innerHTML + adalaber2.innerHTML;
+
