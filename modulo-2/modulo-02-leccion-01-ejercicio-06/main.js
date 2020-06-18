f963b32f946e10f1c54a48091329154247b0480c
Alexandra <sandra.larrosa88@gmail.com>
2020-06-18 18:19:02 +0200

    Ejercicio 6

---
+++ modulo-2/modulo-02-leccion-01-ejercicio-06/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const content = document.querySelector('div');
+
+const title = '<h1>Loren Ipsum</h1>';
+
+const img = '<img src="http://via.placeholder.com/350x150"></img>';
+
+const text = '<p>Loren Ipsum dolor sit amet, consectetur adipisicing elit</p>';
+
+content.innerHTML = title + img + text;
+
+
