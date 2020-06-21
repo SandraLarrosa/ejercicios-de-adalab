3637d63e452ec7735a471fe8da4c57573c2c48c0
Alexandra <sandra.larrosa88@gmail.com>
2020-06-21 12:22:03 +0200

    Ejercicio 8

---
+++ modulo-2/leccion-02/modulo-02-leccion-02-ejercicio-08/main/main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const firstDogImage = 'https://images.dog.ceo/breeds/schipperke/n02104365_8156.jpg';
+const firstDogName = 'Dina';
+
+const secondDogImage = 'https://images.dog.ceo/breeds/collie-border/n02106166_355.jpg';
+const secondDogName = 'Luna';
+
+const thirdDogImage = 'https://images.dog.ceo/breeds/affenpinscher/n02110627_7065.jpg';
+const thirdDogName = 'Lana';
+
+const list = document.querySelector('.list');
+
+list.innerHTML = `<li>
+                <h2>${firstDogName}</h2>
+                <img src=${firstDogImage}>
+                </li>
+                <li>
+                <h2>${secondDogName}</h2>
+                <img src=${secondDogImage}>
+                </li>
+                <li>
+                <h2>${thirdDogName}</h2>
+                <img src=${thirdDogImage}>
+                </li>`;
+
