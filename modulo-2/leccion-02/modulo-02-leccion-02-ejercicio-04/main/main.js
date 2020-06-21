1257681aa532b5e13fc669ab426aee4ac33b0d1f
Alexandra <sandra.larrosa88@gmail.com>
2020-06-21 11:43:39 +0200

    Ejercicio 4

---
+++ modulo-2/leccion-02/modulo-02-leccion-02-ejercicio-04/main/main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const totalPrice = 128;
+
+const totalPerson = 9;
+
+const priceAna = 2;
+
+const total = totalPrice / totalPerson;
+
+console.log(total);
+
+const totalAna = total + priceAna;
+
+console.log(totalAna);
