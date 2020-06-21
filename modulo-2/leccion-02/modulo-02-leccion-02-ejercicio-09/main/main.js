91f64bb3805308d45234cca59304ec367f35b160
Alexandra <sandra.larrosa88@gmail.com>
2020-06-21 12:27:57 +0200

    Ejercicio 9

---
+++ modulo-2/leccion-02/modulo-02-leccion-02-ejercicio-09/main/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const name = 'Alexandra López';
+
+const printName = document.querySelector('.name');
+
+printName.innerHTML = `Mi nombre es ${name} y está compuesto por ${name.length} caracteres.`;
+
