83b19fb2ce46b4ee290e1320ed8e1592c93e2ab9
Alexandra <sandra.larrosa88@gmail.com>
2020-06-23 17:23:16 +0200

    Ejercicio 5. querySelector para todas

---
+++ modulo-2/leccion-04/modulo-02-leccion-04-ejercicio-05/main/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+
+function getEl(a) {
+    return document.querySelector(a);
+}
+
+const div1 = getEl('.div1');
+const div2 = getEl('.div2');
+const div3 = getEl('.div3');
+const div4 = getEl('.div4');
+
+console.log(div4);
