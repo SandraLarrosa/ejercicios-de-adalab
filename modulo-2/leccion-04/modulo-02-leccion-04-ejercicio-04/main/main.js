fb7da9db4c34c09e93e3bff12fb4137b7bf4f2d4
Alexandra <sandra.larrosa88@gmail.com>
2020-06-23 17:12:52 +0200

    Ejercicio 4. Pares o nones

---
+++ modulo-2/leccion-04/modulo-02-leccion-04-ejercicio-04/main/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function evenOrOdd(a) {
+    if (a !== 0) {
+       console.log('El resultado es true');
+    } else {
+       console.log('El resultado es false');
+    }
+}   
+
+evenOrOdd(2);
