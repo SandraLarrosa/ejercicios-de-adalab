24ec0b8fd94260b534a59a5bb6bd4f5ee833fbae
Alexandra <sandra.larrosa88@gmail.com>
2020-06-23 16:41:10 +0200

    Ejercicio 2. Funcion media

---
+++ modulo-2/leccion-04/modulo-02-leccion-04-ejercicio-02/main/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function average(a, b, c, d) {
+    return (a + b + c + d) / 4;
+}
+
+console.log(average(2, 4, 3, 1));
+console.log(average(10, 15, 33, 22));
+console.log(average(100, 250, 430, 98));
