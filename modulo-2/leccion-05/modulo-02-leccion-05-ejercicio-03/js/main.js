82fa4c0ac3a7de19aaf4c0d49d446459f7f3904a
Alexandra <sandra.larrosa88@gmail.com>
2020-06-24 16:42:42 +0200

    Ejercicio 3. Dame ipsum

---
+++ modulo-2/leccion-05/modulo-02-leccion-05-ejercicio-03/js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+
+const text = document.querySelector('.text');
+const text2 = document.querySelector('.text2');
+
+
+
+text.addEventListener('mouseover', () => text2.innerHTML = 'Lorem fistrum hasta luego Lucas la caidita hasta luego Lucas ese que llega ahorarr. Benemeritaar pupita diodeno benemeritaar. No puedor sexuarl me cago en tus muelas ese que llega. Ese pedazo de hasta luego Lucas llevame al sircoo caballo blanco caballo negroorl condemor. Ese que llega torpedo apetecan ese pedazo de pecador condemor tiene musho peligro papaar papaar benemeritaar por la gloria de mi madre.');
+
