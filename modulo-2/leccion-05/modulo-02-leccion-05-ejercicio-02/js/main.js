aaee61fa43b24398690873e9e7aa566e48610721
Alexandra <sandra.larrosa88@gmail.com>
2020-06-24 16:34:00 +0200

    Ejercicio 2. ¿Como te llamas?

---
+++ modulo-2/leccion-05/modulo-02-leccion-05-ejercicio-02/js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+
+const input = document.querySelector('.text');
+const button = document.querySelector('.button');
+
+
+
+button.addEventListener('click', () => console.log(`Hola, ${input.value}`));
+
