85039bea1d94a51c5c0a4904f1a3de1a7d2ab16a
Alexandra <sandra.larrosa88@gmail.com>
2020-06-25 12:10:06 +0200

    Ejercicio 9. Favorite teacher

---
+++ modulo-2/leccion-05/modulo-02-leccion-05-ejercicio-09/js/main.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const isra = document.querySelector('.teacher--isra');
+const barlos = document.querySelector('.teacher--tuerto');
+const nasi = document.querySelector('.teacher--nasi');
+const teacher = document.querySelectorAll('.teacher');
+
+function selected(e){
+  const sectionElement = e.currentTarget;
+    sectionElement.classList.toggle('teacher--selected');
+  
+  if(sectionElement.classList.contains('teacher--selected')) {
+        sectionElement.querySelector('.favorite').innerHTML = 'Quitar'
+    } else {
+        sectionElement.querySelector('.favorite').innerHTML = 'Añàdir'
+    }
+}
+ 
+function getElementTarget() {
+  for(let i = 0; i < teacher.length; i++) { 
+    teacher[i].addEventListener('click', selected);
+  }
+} 
+
+getElementTarget();
+
+/*
+isra.addEventListener('click', selected);
+barlos.addEventListener('click', selected);
+nasi.addEventListener('click', selected);*/
