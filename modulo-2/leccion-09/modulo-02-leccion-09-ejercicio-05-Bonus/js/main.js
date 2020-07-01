dc5aa1879ee6f10ece70c2d264c976339d295a70
Alexandra <sandra.larrosa88@gmail.com>
2020-07-01 20:25:48 +0200

    Ejercicio Bonus. Arbol de Navidad. Ejercicio 6

--- modulo-2/leccion-09/modulo-02-leccion-09-ejercicio-05-Bonus/js/main.js
+++ modulo-2/leccion-09/modulo-02-leccion-09-ejercicio-05-Bonus/js/main.js
@@ -1,11 +1,27 @@
 'use strict';
 
-function tree(number) {
+/* function tree(number) {
   let triangle = '▲';
   for (let i = 0; i < number; i++) {
     console.log(triangle);
     triangle += '▲';
   }
+}
+
+tree(5); 
+
+/* ---------Ejercicio 6--------- */
+
+function tree(number) {
+  const star = '★';
+  const stem = '|';
+  let triangle = '▲';
+  console.log(star);
+  for (let i = 0; i < number; i++) {
+    console.log(triangle);
+    triangle += '▲';
+  }
+  console.log(stem);
 }
 
 tree(5);
