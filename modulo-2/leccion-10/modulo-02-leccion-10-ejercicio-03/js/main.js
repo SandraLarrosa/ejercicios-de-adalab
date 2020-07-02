42a34207627eebc543d0be977d22723c693d851f
Alexandra <sandra.larrosa88@gmail.com>
2020-07-02 17:32:07 +0200

    Buscador de usuarios de GitHub

---
+++ modulo-2/leccion-10/modulo-02-leccion-10-ejercicio-03/js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const button = document.querySelector('.js-button');
+const name = document.querySelector('.js-name');
+const number = document.querySelector('.js-number');
+const img = document.querySelector('.js-image');
+const error = document.querySelector('.js-userError');
+
+function getUser(ev) {
+  ev.preventDefault();
+  const input = document.querySelector('.js-input');
+  const user = input.value;
+  fetch(`https://api.github.com/users/${user}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (user === data.login) {
+        error.classList.add('hidden');
+        name.innerHTML = data.name;
+        number.innerHTML = data.public_repos;
+        img.src = data.avatar_url;
+      } else {
+        error.classList.remove('hidden');
+      }
+    });
+}
+
+button.addEventListener('click', getUser);
